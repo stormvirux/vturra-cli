4bfe488419ed55388fb8f3f690db0947a1518b76
Thaha Muhammed <mthaha1989@gmail.com>
2014-04-02 10:34:46 +0530

    Removed stats module

--- vturra/asys.py
+++ vturra/asys.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
-import statsmodels.api as sm
+#import statsmodels.api as sm
 from numpy.random import randn
 import matplotlib as mpl
 import seaborn as sns
